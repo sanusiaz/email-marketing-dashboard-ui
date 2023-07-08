9b1cf344ca759cbfad0b31ab73e335889b068bd3
Sanusi Abdulazeez <sanusiabdulazeezadebayo1@gmail.com>
2023-07-08 10:37:21 +0100

    Single Subscriber Mail Refactoring

--- src/components/Subscriber/SingleSubscriberMailComponent.vue
+++ src/components/Subscriber/SingleSubscriberMailComponent.vue
@@ -104,9 +104,15 @@
                         </svg>
                     </span>
 
-                    <ButtonComponent @click.prevent="sendMessageToSubscriber()" type="submit"
-                        class="px-5 hover:border-blue-600 hover:text-blue-600 transition-all duration-300 hover:duration-300 shadow-lg hover:bg-white border border-transparent right-1 w-max text-sm py-3 cursor-pointer rounded-md border-gray-100 bg-blue-600 text-white">
-                        Send Message</ButtonComponent>
+
+                    <ButtonComponent @click.prevent="sendMessageToSubscriber()" type="submit" class="px-5 hover:border-blue-600 hover:text-blue-600 transition-all duration-300 hover:duration-300 shadow-lg hover:bg-white border border-transparent right-1 w-max text-sm py-3 cursor-pointer rounded-md border-gray-100 bg-blue-600 text-white">
+                        <div class="flex align-center justify-center align-middle space-x-343,143,0.8)] w-max h-full relative top-0 left-0 right-0">
+                            <svg v-if="this.processingForm" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            </svg> {{  ( this.processingForm  ) ? 'Processing' : 'Send Message'  }}
+                        </div>
+                    </ButtonComponent>
 
                 </div>
 
@@ -154,7 +160,8 @@ export default {
                 template: ''
             },
             stepCount: 1,
-            allTemplates: []
+            allTemplates: [],
+            processingForm: false
         }
     },
 
@@ -232,24 +239,33 @@ export default {
         // Send Message to receivers email
         async sendMessageToSubscriber() {
             this.statusText = this.popupMessage = ''
+            this.processingForm = true
             try {
                 let __response = await axios.post('/subscribers/single/mail', this.formData)
                 if (__response.status === 200) {
                     this.statusText = 'success'
                     this.popupMessage = __response.data.message
-
-                    this.$emit('closeForm', true)
+                    
+                    // Close Form After 5 secs
+                    setTimeout(() => {
+                        this.$emit('closeForm', true)
+                    }, 5000);
                 }
                 else {
                     this.statusText = 'error'
                     this.popupMessage = __response.data.message
                 }
 
+                this.processingForm = false
+
             } catch (error) {
                 error = JSON.parse(error.response.request.response)
                 this.statusText = 'error'
                 this.popupMessage = error.message
+
+                this.processingForm = false
             }
+            
         },
 
         // Get Selected Template Content and parse it to message box
