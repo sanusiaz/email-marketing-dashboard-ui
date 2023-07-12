2448ec5b93b6381089a4c7981eb8012e9929791b
Sanusi Abdulazeez <sanusiabdulazeezadebayo1@gmail.com>
2023-07-12 21:21:31 +0100

    Refactored ShowNewsletter Message to a decentralized route

--- src/components/Newsletter/ShowMessageComponent.vue
+++ src/components/Newsletter/ShowMessageComponent.vue
@@ -2,11 +2,11 @@
     <div class="main_contents pt-5 px-10 pb-5 bg-gray-400 h-screen overflow-auto ">
 
         <div class="m-auto md:max-w-[700px] bg-white p-2 rounded">
-            <router-link :to="{ name: 'admin-newsletter-show', params: {id: this.newsletterId} }"
+            <router-link :to="{ name: 'admin-news   letter-show', params: { id: this.newsletterId } }"
                 class="font-semibold font-OpenSans py-2 relative w-max text-sm text-white bg-blue-600 rounded p-1 px-4 flex gap-2 align-middle items-center duration-200 hover:duration-200 hover:bg-white border border-blue-700 hover:shadow-md hover:text-blue-700 mb-3">
                 <i class="fas fa-chevron-left"></i>
                 <span>Back</span>
-            </router-link>
+            </router-link>  
             <div v-html="this.message" class="overflow-auto block relative"></div>
         </div>
     </div>
@@ -31,7 +31,18 @@ export default {
             try {
                 let __response = await axios.get(`/newsletters/${id}/message`)
                 this.message = __response.data.message
-                console.log(__response)
+                let __uuid = __response.data.uuid;
+
+                if (this.message === null) {
+                    try {
+                        let response = await axios.get(`/templates/${__uuid}`)
+                        if (response.status === 200) {
+                            this.message = response.data
+                        }
+                    } catch (error) {
+                        console.log(error)
+                    }
+                }
             } catch (error) {
                 console.error(error)
             }
